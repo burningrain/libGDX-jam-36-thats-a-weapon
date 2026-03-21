300d7ebea47084ac7237f8304795e16bcaff92d1
ash <lixkram@gmail.com>
2026-03-22 00:36:01 +0300

    - added documents

--- core/src/main/java/com/github/br/libgdx/jam36/screens/MainScreen.java
+++ core/src/main/java/com/github/br/libgdx/jam36/screens/MainScreen.java
@@ -11,11 +11,10 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.github.br.libgdx.jam36.Constants;
 import com.github.br.libgdx.jam36.CustomOrthogonalTiledMapRenderer;
-import com.github.br.libgdx.jam36.context.GameContext;
 import com.github.br.libgdx.jam36.Resources;
+import com.github.br.libgdx.jam36.context.GameContext;
 import com.github.br.libgdx.jam36.context.TabletContext;
 import com.github.br.libgdx.jam36.screens.phase.*;
-import com.github.br.libgdx.jam36.screens.phase.predicate.PhasePredicate;
 import com.github.br.libgdx.jam36.screens.phase.predicate.PredicatePhase;
 import structure.screen.AbstractGameScreen;
 
@@ -56,33 +55,58 @@ public class MainScreen extends AbstractGameScreen {
 
     private GameContext createGameContext() {
         Array<Phase> phases = new Array<>();
+
+        phases.add(new HrDialogPhase("Раз уж пересеклись, подпиши уже, пожалуйста, свои документы"));
         phases.add(new ChangeContextPhase(context -> {
             TabletContext tabletContext = context.getTabletContext();
             tabletContext.setPages(
-                1,
-                "это первая страница",
-                "это вторая страница",
-                "это третья страница",
-                "это четвертая страница"
+                0,
+                "Вы подписываете следующие документы:\n" +
+                    "\n- Политика информационной безопасности" +
+                    "\n- Согласие на участие в корпоративной лотерее" +
+                    "\n- Дополнения в правилах внутреннего трудового распорядка" +
+                    "\n- Изменения в кодексе деловой этики" +
+                    "\n- Дополнения в политике конфликта интересов" +
+                    "\n- Положение о цвете носков и галстуков"
+                ,
+                "- Согласие на слежку через корпоративную веб-камеру" +
+                    "\n- Положение по надлежащему завершению рабочего дня" +
+                    "\n- Запрет на установку обоев рабочего стола, не прошедших утверждение арт-директором" +
+                    "\n- Акт о невозвратном потреблении печенек из корпоративной кладовки" +
+                    "\n- Соглашение о неразглашении рецепта кофе из кофемашины" +
+
+                    "\n- Акт приема-передачи единственной удлиненной розетки (с правом наследования)"
+                ,
+                "- Должностная инструкция \"Специалист по синергии\"" +
+                    "\n- Кодекс использования эмодзи в рабочем чате" +
+                    "\n- Инструктаж по защите от сквозняков" +
+                    "\n- Заявление на увольнение по собственному желанию" +
+                    "\n- Памятка по противодействию страху в отчетах наверх" +
+                    "\n- Запрет на использование RGB-подсветки клавиатуры в режиме \"дискотека\" после полуночи"
+                ,
+                "- Обязательство не кормить хомяков, на которых крутится сервер продакшена" +
+                    "\n- Отказ от претензий по поводу нехватки розеток " +
+                    "\n- Обязательство не коммитить код после 03:00 без подтверждения дежурным психиатром" +
+                    "\n- Акт приема-передачи бессонницы (приложение к график-менеджменту)" +
+                    "\n- Материальная ответственность за настроение начальства" +
+                    "\n- Обязательство не приносить в офис настольные игры длиннее 30 минут"
             );
             tabletContext.setSignButtonText("Ознакомиться\nи подписать");
         }));
 
         phases.add(new TabletPhase()); // выбор языка, имени-фамилии и прочее
-        phases.add(new HrDialogPhase("Здравствуй, я очень рада с тобой познакомиться! Хочеть выпить чаю?"));
+        phases.add(new HrDialogPhase("А знаешь что? Давай мы начнем заново. Будешь чай?"));
         phases.add(new MindChooserPhase(
                 actorFactory,
                 new Choose(1, "Буду,\nблагодарю!"),
                 new Choose(2, "Спасибо,\nне нужно"),
                 new Choose(3, "Я отказываюсь"),
-                new Choose(4, "Только после вас")
+                new Choose(4, "Сразу после вас")
             )
         );
         phases.add(new GoToTheHellPhase());
+        phases.add(new GameOverPhase());
 
-        phases.add(new ChangeContextPhase(context -> {
-            context.setGameOverAndNeedChangePhases(true);
-        }));
 
 //        phaseManager.addPhase(new HrMeetingPhase());
 //        phaseManager.addPhase(new ThoughtPhase());
